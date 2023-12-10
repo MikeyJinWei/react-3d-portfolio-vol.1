2f1b43021600f4bb93f48bf21ab5ffed810f713f
MikeyJ <currygeigei@gmail.com>
2023-12-11 02:25:25 +0800

    feature: build 3d rotate functionality

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -12,10 +12,61 @@ import { useFrame, useThree } from '@react-three/fiber';
 import islandScene from '../assets/3d/island.glb';
 import { a } from '@react-spring/three';
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props }) => {
   const islandRef = useRef();
 
+  const { gl, viewport } = useThree();
   const { nodes, materials } = useGLTF(islandScene);
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    lastX.current = clientX;
+  };
+
+  const handlePointerMove = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+
+    if (isRotating) handlePointerUp(e);
+  };
+
+  const handlePointerUp = (e) => {
+    e.stopProgation();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX;
+
+    const delta = (clientX - lastX.current) / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+
+    lastX.current = clientX;
+
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  };
+
+  useEffect(() => {
+    document.addEventListener('pointerdown', handlePointerDown);
+    document.addEventListener('pointermove', handlePointerMove);
+    document.addEventListener('pointerup', handlePointerUp);
+
+    return () => {
+      document.removeEventListener('pointerdown', handlePointerDown);
+      document.removeEventListener('pointermove', handlePointerMove);
+      document.removeEventListener('pointerup', handlePointerUp);
+    };
+  }, [gl, handlePointerDown, handlePointerMove, handlePointerUp]);
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,10 +10,11 @@ import Plane from '../models/Plane';
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
 
+  // adjust Island
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
     let screenPosition;
-    let islandRotation = [0.1, 4.7, 0];
+    let rotation = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9];
@@ -23,11 +24,29 @@ const Home = () => {
       screenPosition = [0, -6.5, -43];
     }
 
-    return [screenScale, screenPosition, islandRotation];
+    return [screenScale, screenPosition, rotation];
+  };
+
+  // adjust Plane
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+    // let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+
+    return [screenScale, screenPosition, rotation];
   };
 
   const [islandScale, islandPosition, islandRotation] =
     adjustIslandForScreenSize();
+
+  const [planeScale, planePosition] = adjustIslandForScreenSize();
 
   return (
     /* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -55,8 +74,15 @@ const Home = () => {
             scale={islandScale}
             position={islandPosition}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
-          <Plane />
+          <Plane
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+            isRotating={isRotating}
+          />
         </Suspense>
       </Canvas>
     </section>
