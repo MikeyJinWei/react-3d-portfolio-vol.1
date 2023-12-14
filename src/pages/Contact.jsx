23787a210cef304d0ad7b3e5e7f8187555d1bbb7
MikeyJ <currygeigei@gmail.com>
2023-12-15 00:37:34 +0800

    feature: building form functionality wip

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const Contact = () => {
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' });
-  useState;
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleChange = () => {};
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+      [e.target.email]: e.target.value,
+      [e.target.message]: e.target.value,
+    });
+  };
 
   const handleFocus = () => {};
 
   const handleBlur = () => {};
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
 
-        <form className='w-full flex flex-col gap-7 mt-14'>
+        <form
+          onSubmit={handleSubmit}
+          className='w-full flex flex-col gap-7 mt-14'
+        >
           <label className='text-black-500 font-semibold'>
             Name
             <input
