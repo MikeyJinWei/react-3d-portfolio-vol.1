d3ebf8d76dfca5776600dce457e0c194cd179e9a
MikeyJ <currygeigei@gmail.com>
2023-12-20 22:17:44 +0800

    feature: building page wip

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>
+        Hello, I'm{' '}
+        <span className='blue-gradient_text font-semibold drop-shadow'>
+          Mikey
+        </span>
+      </h1>
+
+      <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+        <p>
+          Software Developer based in Taiwan, finding myself obsessing with
+          frontend skills through hands-on learning and building applications.
+        </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>My Skills</h3>
+      </div>
+    </section>
+  );
 };
 
 export default About;
