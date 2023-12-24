3d0bdd1519c61c38a538032e5eb2b4a751e77b77
MikeyJ <currygeigei@gmail.com>
2023-12-24 23:30:38 +0800

    fix: set full height for desktop screen

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -75,7 +75,7 @@ const Contact = () => {
   };
 
   return (
-    <section className='relative flex lg:flex-row flex-col max-container bg-[#202023] sm:h-[100vh] xl:h-full'>
+    <section className='relative flex lg:flex-row flex-col max-container bg-[#202023] h-full lg:h-screen'>
       {alert.show && <Alert {...alert} />}
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text text-[#eeeeee]'>與我聯絡</h1>
