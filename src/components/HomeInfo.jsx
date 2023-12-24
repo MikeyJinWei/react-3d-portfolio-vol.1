a604a437b640a8a76456b3584542767e11635b5a
MikeyJ <currygeigei@gmail.com>
2023-12-24 17:03:43 +0800

    feature: modify content

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -4,7 +4,7 @@ import { arrow } from '../assets/icons';
 
 const InfoBox = ({ text, link, btnText }) => (
   <div className='info-box'>
-    <p className='font-medium sm: text-xl text-center '>{text}</p>
+    <p className='font-medium sm: text-xl text-center'>{text}</p>
     <Link to={link} className='neo-brutalism-white neo-btn'>
       {btnText}
       <img src={arrow} className='w-4 h-4 object-contain' />
@@ -17,28 +17,29 @@ const renderContent = {
     <h1 className='sm: text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
       Hi, 我是 <span className='font-semibold'>Mikey</span> 👋
       <br />
-      我是一個前端開發者
+      我是一名前端開發人員
     </h1>
   ),
   2: (
-    <InfoBox
-      text='我自學前端開發的技術並正在持續精進自己的技能'
-      link='/about'
-      btnText='Learn more'
-    />
+    <h1 className='sm: text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      我自學前端開發的技術並且，
+      <br />
+      <span className='font-semibold gold-gradient_text'>每天</span>
+      都在持續精進自己的技能
+    </h1>
   ),
   3: (
     <InfoBox
-      text='對我的作品感到好奇嗎? 歡迎參觀我的作品集 😃'
+      text='對我的作品感到好奇嗎？歡迎參觀我的作品集😃'
       link='/projects'
-      btnText='參觀我的作品集'
+      btnText='前往參觀'
     />
   ),
   4: (
     <InfoBox
-      text='正在尋找前端開發者嗎？ 我就近在咫尺喔 📲'
+      text='正在尋找前端開發人員嗎？或許我就近在咫尺喔 📲'
       link='/contact'
-      btnText="Let's talk"
+      btnText='期盼你的聯絡'
     />
   ),
 };
