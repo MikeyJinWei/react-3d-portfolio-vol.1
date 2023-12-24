c97361e7189efbb40205b9d4e03375f63d1815c7
MikeyJ <currygeigei@gmail.com>
2023-12-24 22:33:33 +0800

    fix: set full height

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
   return (
-    <main className='bg-[#202023] h-[100vh]'>
+    <main className='bg-[#202023]'>
       <Router>
         <Navbar />
         <Routes>
