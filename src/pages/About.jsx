412a386c797cab72b2ebc7bed6dd25ad4805fff6
MikeyJ <currygeigei@gmail.com>
2024-01-21 17:32:24 +0800

    feature: update self-introduction content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
         </span>
       </h1>
 
-      <div className='mt-5 flex flex-col gap-3 text-slate-300 text-xl'>
+      <div className='mt-5 flex flex-col gap-3 md:text-xl text-slate-300'>
         <p>
           我是一名前端開發者，在求學和就業的旅途中，我透過一次次的實作開始對網頁開發產生興趣，並每天致力於進步
         </p>
@@ -109,10 +109,9 @@ const About = () => {
 
       <div className='py-16'>
         <h3 className='subhead-text'>工作經驗</h3>
-        <div className='mt-5 flex flex-col gap-3 text-xl text-slate-300'>
+        <div className='mt-5 flex flex-col gap-3 md:text-xl text-slate-300'>
           <p>
-            我認真看待每一份工作，秉持著「見賢思齊，
-            見不賢而內自省」的心態；面對工作，我偏好理性的思考，並在溝通及考量各種因素後做出最佳的選擇：
+            我認真看待每一份工作，秉持著「見賢思齊」的心態；面對工作，我偏好理性的思考，並在溝通及考量各種因素後做出最佳的決策：
           </p>
         </div>
         <div className='mt-12 flex'>
